9d55643b1ec5fba8bbc9ee115cf18d415104c5db
Zac Childress <60277286+SixMode@users.noreply.github.com>
2020-11-20 23:29:51 -0600

    Add files via upload
    My very first program. 11/20/20

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    int number;
+    std::cin >> number;
+    std::cout << "Amazing! " << number << " is my favorite number too!" << std::endl;
+    
+    return 0;
+}
