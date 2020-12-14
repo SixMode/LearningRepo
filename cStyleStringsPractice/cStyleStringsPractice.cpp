c88b89e684b1385e9cee2a542543292f1dd20771
Zac Childress <60277286+SixMode@users.noreply.github.com>
2020-12-14 11:18:24 -0600

    Add files via upload
    Learning to use C-Style strings.

---
+++ cStyleStringsPractice/cStyleStringsPractice.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    char firstName[20]{};
+    char lastName[20]{};
+    char fullName[50]{};
+    char temp[50]{};
+
+ 
+
+    cout << "Enter your mom's full name: ";
+    cin.getline(fullName, 50);
+    cout << "Your mom's full name is " << fullName << endl;
+
+
+
+}
